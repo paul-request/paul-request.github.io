e903185646843f7df440ce0f19772ca717c235c4
pmbennett <paul@pmbennett.net>
2019-05-29 10:39:43 +0100

    Updates

---
+++ precache-manifest.4474dd3cab1bb3074a929b009fc42449.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "979efc18d9d45c9fd5fb",
+    "url": "/static/css/main.46f97048.chunk.css"
+  },
+  {
+    "revision": "979efc18d9d45c9fd5fb",
+    "url": "/static/js/main.979efc18.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "add590cf4376c02374fb",
+    "url": "/static/js/2.add590cf.chunk.js"
+  },
+  {
+    "revision": "cdffdb9f6bc255d89ffb03e49c803ac8",
+    "url": "/static/media/raindrop.cdffdb9f.svg"
+  },
+  {
+    "revision": "cc4c937f9ac63e7bba97c7de04d4e803",
+    "url": "/index.html"
+  }
+];
